8c13116597a8c642a3b8f45e9eaabfd459940f80
nyel-101 <dandevjr@gmail.com>
2025-03-04 11:46:59 +0000

    chore(config): integrate middleware routes

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f34610d11ea343c0b591e8659d12dba1 = () => {
-  return '2026-05-05T05:25:47Z';
+const node_a694e3d4d0f2486186e781dcdc6b161b = () => {
+  return '2025-03-04T11:46:59Z';
 };
