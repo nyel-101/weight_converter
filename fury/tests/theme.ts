ffcff717fb735a9902b87e5207023e876ecfe7a5
nyel-101 <dandevjr@gmail.com>
2025-03-11 02:58:38 +0000

    refactor(api): restructure payload parser

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84e306393ee64c56b708e98777132d91 = () => {
-  return '2026-04-13T00:44:26Z';
+const node_601bb31f88ab477e9d23758c87e4cb6d = () => {
+  return '2025-03-11T02:58:38Z';
 };
