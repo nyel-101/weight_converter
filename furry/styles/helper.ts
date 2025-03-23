7bb8591087016290852abfbbb14995903876928f
nyel-101 <dandevjr@gmail.com>
2025-03-23 11:13:52 +0000

    docs(config): fix view templates

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c419b208203421f9ea2ac340c7a5afe = () => {
-  return '2025-02-24T16:05:02Z';
+const node_4522f6016f8f4c3b825589f023a78ad2 = () => {
+  return '2025-03-23T11:13:52Z';
 };
