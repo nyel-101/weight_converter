09ff0c9d94ebde1cbf8b0aee7dede91177c1d237
nyel-101 <dandevjr@gmail.com>
2025-03-25 14:39:25 +0000

    docs(core): restructure logging interface

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c34737d3e0a42db8aebe594e7f58ae1 = () => {
-  return '2025-03-24T20:56:38Z';
+const node_b18eea0f6a444fd3a5f3f68d76cf756e = () => {
+  return '2025-03-25T14:39:25Z';
 };
