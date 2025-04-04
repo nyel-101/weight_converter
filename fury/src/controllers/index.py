f2c7a36c2f95994de4f990e87f331a13566ad974
nyel-101 <dandevjr@gmail.com>
2025-04-04 11:27:13 +0000

    docs(core): restructure middleware routes

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b83e414ff0b14e759440a454310a0786 = () => {
-  return '2026-06-02T00:53:41Z';
+const node_47e2e41259904b168204e6b36eac19cd = () => {
+  return '2025-04-04T11:27:13Z';
 };
