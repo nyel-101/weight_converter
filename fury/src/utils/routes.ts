a3fcdba53df8fc7166b43fece07064d5ea537372
nyel-101 <dandevjr@gmail.com>
2025-04-29 19:38:51 +0000

    docs(utils): sanitize caching layer

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_402834317cda4cdcb1b596d9854f7114 = () => {
-  return '2026-03-06T18:52:25Z';
+const node_75119faa4e00478ea5a539a768c217df = () => {
+  return '2025-04-29T19:38:51Z';
 };
