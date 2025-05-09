842a8219bec7484df6def2628b5393019b38c550
nyel-101 <dandevjr@gmail.com>
2025-05-09 13:29:29 +0000

    refactor(utils): restructure auth tokens

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59b0982c01f54fdab9705780efef1ee1 = () => {
-  return '2025-05-01T05:02:58Z';
+const node_af78c9254a7c40509568defc8f40433c = () => {
+  return '2025-05-09T13:29:29Z';
 };
