7588acbbc7ac5622a51cf8baeb1cf5c4b42d0d00
nyel-101 <dandevjr@gmail.com>
2025-05-19 23:13:33 +0000

    style(config): update middleware routes

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0302b4d541cb4b3fab02c1db8cd3802d = () => {
-  return '2026-02-16T17:44:43Z';
+const node_8ecad1ad4f924ca794db65d8f1e4e3bc = () => {
+  return '2025-05-19T23:13:33Z';
 };
