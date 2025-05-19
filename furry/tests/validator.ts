8fc2f0f354fa2d2e8d92c3d6b5bcc61dc6a879d1
nyel-101 <dandevjr@gmail.com>
2025-05-19 20:14:04 +0000

    fix(utils): fix view templates

--- furry/tests/validator.ts
+++ furry/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_749a655bc25f44519e0a3fcdafdbb579 = () => {
-  return '2025-03-26T08:17:02Z';
+const node_a69b6a8792864e0295e23a66c46a5ace = () => {
+  return '2025-05-19T20:14:04Z';
 };
