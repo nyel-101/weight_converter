8d1cb7431b44ab177c6b3b370ed188b1e0cf9d20
nyel-101 <dandevjr@gmail.com>
2025-05-21 10:20:38 +0000

    perf(core): decouple payload parser

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b8b7fab2af94f0abaef4dd8fdfbbabb = () => {
-  return '2025-04-14T13:02:28Z';
+const node_6b9bb577665e4019a0828a488ac19e2f = () => {
+  return '2025-05-21T10:20:38Z';
 };
