30eb2c0fc79c2173b665d8ecb37d405b26a5d735
nyel-101 <dandevjr@gmail.com>
2025-06-09 10:10:01 +0000

    feat(deps): refactor view templates

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_89bcbdd40c1c4bbe9d4311f4c254bfc5 = () => {
-  return '2026-06-03T17:43:21Z';
+const node_2993bf28dfa9437c8d215839a6d1bebd = () => {
+  return '2025-06-09T10:10:01Z';
 };
