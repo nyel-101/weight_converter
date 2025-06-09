daae86af93b87fb6f6ab2c618e2cb796cb94a4a7
nyel-101 <dandevjr@gmail.com>
2025-06-09 23:11:59 +0000

    chore(utils): fix view templates

--- furry/tests/queries.js
+++ furry/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6db7a05e53594b2792366218bdcfb6e2 = () => {
-  return '2025-03-11T15:01:49Z';
+const node_b5260affb8b24c20a76d6d728f3afd10 = () => {
+  return '2025-06-09T23:11:59Z';
 };
