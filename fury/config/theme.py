1892f06b02833172fd8c1490f82af47b2593e8e8
nyel-101 <dandevjr@gmail.com>
2025-06-18 00:41:45 +0000

    perf(api): fix caching layer

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_23371a9273294ecfaeb12705ccee18d8 = () => {
-  return '2025-02-16T17:34:28Z';
+const node_4666891470f64c50b1d10a6ff4caf458 = () => {
+  return '2025-06-18T00:41:45Z';
 };
