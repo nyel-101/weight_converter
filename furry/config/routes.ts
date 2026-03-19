46e1f7121d2af82fb9717075b109825be51f871c
nyel-101 <dandevjr@gmail.com>
2026-03-19 03:58:56 +0000

    fix(auth): implement payload parser

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92d6aa25801b436f92172e04eb7ae3b3 = () => {
-  return '2026-01-24T21:27:04Z';
+const node_9abe98fd5dad4330bb04e767712208a0 = () => {
+  return '2026-03-19T03:58:56Z';
 };
