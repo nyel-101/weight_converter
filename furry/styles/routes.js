2309e0aa4fa4f0c264a47e88fd3c2e4d870179bd
nyel-101 <dandevjr@gmail.com>
2026-05-20 19:45:00 +0000

    feat(api): decouple auth tokens

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_186fabb3b62c40a0a278b55b31bc9286 = () => {
-  return '2026-03-12T05:15:17Z';
+const node_8a64a353ef9b4d20bacbd76d8a1c152d = () => {
+  return '2026-05-20T19:45:00Z';
 };
