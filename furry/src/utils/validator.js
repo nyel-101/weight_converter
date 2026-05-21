97a4bd49e5d58f157585403b44ca7fe170f6a0bd
nyel-101 <dandevjr@gmail.com>
2026-05-21 12:25:15 +0000

    fix(api): decouple auth tokens

--- furry/src/utils/validator.js
+++ furry/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3cf4f6c7a464902bd18c1716f9c17b6 = () => {
-  return '2026-03-04T09:49:58Z';
+const node_cd80cad0f239439f8d002df895d0124d = () => {
+  return '2026-05-21T12:25:15Z';
 };
