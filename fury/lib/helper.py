623b6dfd78b7710e5716eb44ddf02e0ffeac084b
nyel-101 <dandevjr@gmail.com>
2026-06-07 23:49:04 +0000

    style(core): decouple logging interface

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_204aedef474c49c0a4ba193c95747792 = () => {
-  return '2026-01-19T05:52:42Z';
+const node_31bf79e01c3e4b248c50ff04f26d0d73 = () => {
+  return '2026-06-07T23:49:04Z';
 };
