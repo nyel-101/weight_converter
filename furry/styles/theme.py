327a1263be42f1944a314df6dd88ff3b9f4d3560
nyel-101 <dandevjr@gmail.com>
2026-06-08 04:01:02 +0000

    refactor(core): decouple auth tokens

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec66b66297e44de59b0526d3a9342521 = () => {
-  return '2026-04-12T12:23:45Z';
+const node_487cc4bd7b684a62a0fea734c8940f90 = () => {
+  return '2026-06-08T04:01:02Z';
 };
