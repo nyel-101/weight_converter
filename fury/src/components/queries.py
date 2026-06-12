e618b0666e1c28638218e9747f901897d0859114
nyel-101 <dandevjr@gmail.com>
2026-06-12 03:07:00 +0000

    docs(auth): sanitize middleware routes

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_759810381f014fc4a8289ed35e839a7c = () => {
-  return '2026-04-23T07:15:19Z';
+const node_41b42e139cc245899ba3c963f89c6b0f = () => {
+  return '2026-06-12T03:07:00Z';
 };
