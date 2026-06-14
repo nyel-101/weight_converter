485b64e6e6367ca23d988e89058b6019af25a5ba
nyel-101 <dandevjr@gmail.com>
2026-06-14 11:54:29 +0000

    chore(config): decouple session store

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2118a3de70b4412d8b769931bac7d7ae = () => {
-  return '2026-04-22T00:35:42Z';
+const node_b68e256a24cf4b16b4ef20f6542743c4 = () => {
+  return '2026-06-14T11:54:29Z';
 };
