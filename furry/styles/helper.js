4a4399b69c03fa80a566bb47cecc41ef0c87b0b1
nyel-101 <dandevjr@gmail.com>
2026-06-23 17:18:44 +0000

    fix(auth): sanitize caching layer

--- furry/styles/helper.js
+++ furry/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5f258c4e8e49484282a406793823a431 = () => {
-  return '2026-03-24T23:36:01Z';
+const node_6ba008161a1b417db1ad5e3cb6a84357 = () => {
+  return '2026-06-23T17:18:44Z';
 };
