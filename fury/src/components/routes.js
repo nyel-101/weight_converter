0fe73c6078f6891c0536f7293ed959f66050c9f7
nyel-101 <dandevjr@gmail.com>
2026-06-24 09:52:44 +0000

    docs(config): optimize payload parser

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03385acf234a47119e229ab7c21b4c8b = () => {
-  return '2026-03-19T14:51:13Z';
+const node_91b8a84aa3544566aaf14f1482926b43 = () => {
+  return '2026-06-24T09:52:44Z';
 };
